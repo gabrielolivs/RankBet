e06bf4dd394caff49cf50cc0e3eef5eb1ccec5d7
Leonardo Marinho de Melo Júnior <41721777+Leommjr@users.noreply.github.com>
2023-05-29 20:27:27 -0300

    Create BettingApi.java

---
+++ src/main/java/br/com/rankbet/model/api/BettingApi.java
@@ -0,0 +1,8 @@
+package br.com.rankbet.model.api;
+
+public class BettingApi {
+
+    public static final String API_TOKEN = "?token=bd207bc594534134b9c38e54847eb956aeab3bc378b54411bdc5245e1272b4af";
+    public static final String LIVE_ENDPOINT = "/live/all";
+    
+}
