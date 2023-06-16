a8109dd63bd8e3efe6a8e0655b995e2fb1407db7
EstevaoFCardoso <estevaof10@gmail.com>
2023-06-16 13:47:13 -0300

    Alteração pasta referente ao css da app

--- src/main/java/br/com/rankbet/controller/AutenticationBean.java
+++ src/main/java/br/com/rankbet/controller/AutenticationBean.java
@@ -2,13 +2,14 @@ package br.com.rankbet.controller;
 
 import br.com.rankbet.model.dto.UserDTO;
 import br.com.rankbet.service.LoginService;
-import br.com.rankbet.service.UserService;
+import jakarta.annotation.ManagedBean;
 import jakarta.annotation.PostConstruct;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Named;
 
 @Named
 @ViewScoped
+@ManagedBean
 public class AutenticationBean {
 
     public UserDTO userDTO;
