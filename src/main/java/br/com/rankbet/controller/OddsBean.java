2e835477044c06f5a03e928c15b4f6220d00b983
Leommjr <leommjr09@gmail.com>
2023-06-16 22:49:02 -0300

    create redirect

--- src/main/java/br/com/rankbet/controller/OddsBean.java
+++ src/main/java/br/com/rankbet/controller/OddsBean.java
@@ -4,6 +4,7 @@ import br.com.rankbet.model.game.Game;
 import br.com.rankbet.service.LiveGamesService;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.ExternalContext;
 import jakarta.faces.context.FacesContext;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
@@ -38,11 +39,16 @@ public class OddsBean {
     }
 
     public void openNewTab(String url) {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = facesContext.getExternalContext();
+
         try {
-            FacesContext.getCurrentInstance().getExternalContext().redirect(url);
+            externalContext.redirect(url);
         } catch (IOException e) {
             e.printStackTrace();
             // Handle the exception if necessary
         }
+
+        facesContext.responseComplete();
     }
 }
