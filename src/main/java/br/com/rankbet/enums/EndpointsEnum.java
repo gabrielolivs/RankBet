32ad0f9719351bae5ab06c399ad11eb429b22f79
Leommjr <leommjr09@gmail.com>
2023-06-16 22:40:38 -0300

    create api proxy

--- src/main/java/br/com/rankbet/enums/EndpointsEnum.java
+++ src/main/java/br/com/rankbet/enums/EndpointsEnum.java
@@ -1,14 +1,14 @@
 package br.com.rankbet.enums;
 
 public enum EndpointsEnum {
-    XBET("http://api.rankbet.tech/1xbet"),
-    SBOBET("http://api.rankbet.tech/sbobet"),
-    BETWAY("http://api.rankbet.tech/betway"),
-    GGBET("http://api.rankbet.tech/ggbet"),
-    FONBET("http://api.rankbet.tech/fonbet"),
-    MARATHONBET("http://api.rankbet.tech/marathonbet"),
-    PINNACLE("http://api.rankbet.tech/pinnacle"),
-    PARIMATCH("http://api.rankbet.tech/parimatch");
+    XBET("http://api.rankbet.tech:8000/1xbet"),
+    SBOBET("http://api.rankbet.tech:8000/sbobet"),
+    BETWAY("http://api.rankbet.tech:8000/betway"),
+    GGBET("http://api.rankbet.tech:8000/ggbet"),
+    FONBET("http://api.rankbet.tech:8000/fonbet"),
+    MARATHONBET("http://api.rankbet.tech:8000/marathonbet"),
+    PINNACLE("http://api.rankbet.tech:8000/pinnacle"),
+    PARIMATCH("http://api.rankbet.tech:8000/parimatch");
     public String endpoint;
     EndpointsEnum(String endpoint) {
         this.endpoint = endpoint;
